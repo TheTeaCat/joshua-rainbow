2bad676d0c07630e88dde68c3f0794593e719cae
Joshua O'sullivan <joinks1999@gmail.com>
2019-09-01 13:28:46 +0100

    Added some old school work and made landscape items in gallery flexbox take twice the width

--- js/smartgallery.js
+++ js/smartgallery.js
@@ -1,5 +1,5 @@
-function generateImageHTML(imageURL) {
-    return "<div class=\"gallery-image-box\"><img src=\"" + imageURL + "\"></div>"
+function generateImageHTML(imageURL, i) {
+    return "<div class=\"gallery-image-box\"><img id=\"img" + i + "\" src=\"" + imageURL + "\"></div>"
 }
 
 window.addEventListener("load", function () {
@@ -21,12 +21,25 @@ window.addEventListener("load", function () {
     if (gallery != null) {
         URL = "img/" + gallery.replace(".","/");
 
+        function getOrientationChecker(img) { 
+            return function() {
+                console.log("toot");
+                if (img.width > img.height) {
+                    img.parentNode.className = "gallery-image-box-landscape";
+                }
+            }
+        }
+        
         function getNextImage (URL, i) {
             var img = new Image();
             var imgURL = URL+"/"+i+".jpg";
             img.addEventListener("load",function() {
                 console.log("Got", imgURL);
-                galleryElement.innerHTML += generateImageHTML(imgURL);
+                galleryElement.innerHTML += generateImageHTML(imgURL, i);
+
+                var img = document.getElementById("img"+i);
+                img.addEventListener("load", getOrientationChecker(img));
+
                 getNextImage(URL,i+1);
             })
             img.addEventListener("error", function() {
