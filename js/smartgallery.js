cf21fe141d99f656baa29a834516d1f5c46df27b
Joshua O'sullivan <joinks1999@gmail.com>
2019-09-01 02:08:04 +0100

    Added more images and made a subtle change to smartgallery.js so it doesn't bother looking for more than one image for the index page.

--- js/smartgallery.js
+++ js/smartgallery.js
@@ -6,28 +6,30 @@ window.addEventListener("load", function () {
     var url = new URL(document.URL);
     var gallery = url.searchParams.get("gallery");
 
+    var galleryElement = document.getElementById("gallery")
+    galleryElement.innerHTML = "";    
+
     if (gallery != null) {
         URL = "img/" + gallery.replace(".","/");
+
+        function getNextImage (URL, i) {
+            var img = new Image();
+            var imgURL = URL+"/"+i+".jpg";
+            img.addEventListener("load",function() {
+                console.log("Got", imgURL);
+                galleryElement.innerHTML += generateImageHTML(imgURL);
+                getNextImage(URL,i+1);
+            })
+            img.addEventListener("error", function() {
+                console.log("End of images.");
+            })
+            img.src = imgURL;
+        }
+    
+        getNextImage(URL, 1);
+    
     } else {
-        URL = "img";
+        galleryElement.innerHTML += generateImageHTML("img/index.jpg");
     }
 
-    var gallery = document.getElementById("gallery")
-    gallery.innerHTML = "";
-
-    function getNextImage (URL, i) {
-        var img = new Image();
-        var imgURL = URL+"/"+i+".jpg";
-        img.addEventListener("load",function() {
-            console.log("Got", imgURL);
-            gallery.innerHTML += generateImageHTML(imgURL);
-            getNextImage(URL,i+1);
-        })
-        img.addEventListener("error", function() {
-            console.log("End of images.");
-        })
-        img.src = imgURL;
-    }
-
-    getNextImage(URL, 1);
 })
