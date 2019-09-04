f4a330743dd244255d2c1ae4fd2ad7f8da1fba80
Joshua O'sullivan <joinks1999@gmail.com>
2019-09-04 16:32:41 +0100

    Modified nav elements margins

--- js/collapsibles.js
+++ js/collapsibles.js
@@ -4,17 +4,17 @@ window.addEventListener("load", function () {
     var contents = document.getElementsByClassName("collapsible-content");
 
     function toggleCollapsible(i) {
-        console.log(i);
-        console.log(contents[i].style.display);
         if (contents[i].style.display == "block") {
             collapsibles[i].innerHTML = collapsibles[i].innerHTML.replace("-","+");
             collapsibles[i].style.background = "#c8c8c8";
             contents[i].style.display = "none";
+            contents[i].parentNode.style.display = "inline-block";
             localStorage.setItem("Collapsible #"+i, false);        
         } else {
             collapsibles[i].innerHTML = collapsibles[i].innerHTML.replace("+","-");
             collapsibles[i].style.background = "#fff";
             contents[i].style.display = "block";
+            contents[i].parentNode.style.display = "block";
             localStorage.setItem("Collapsible #"+i, true);        
         }
     }
